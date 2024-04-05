82a08ee9e093fc7153b5ab3e027c2a3de161a76e
MariaPiCruz <cruzmariadelpilar73@gmail.com>
2024-04-05 18:41:24 -0300

    responsive corregido

--- js/script.js
+++ js/script.js
@@ -63,3 +63,21 @@ function eventoMenu(){
         btnMnu.innerHTML='≡';
 	};
 };
+
+/**************************************************/
+/* Detectar el cambio de tamaño de la ventana     */
+/**************************************************/
+window.addEventListener('resize', function() {
+    // Obtener el nuevo ancho de la ventana
+    var nuevoAncho = window.innerWidth;
+    const menu = document.querySelector('#menu');
+    // Hacer lo que necesites con el nuevo ancho
+    console.log("El ancho de la ventana ha cambiado a: " + nuevoAncho);
+    if(nuevoAncho>1000){
+        menu.style.transform = 'translateX(0%)';
+		contador=0;
+    }else{
+        menu.style.transform = 'translateX(-120%)';
+        contador=1;
+    }
+});
