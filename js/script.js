602b6bf598ce8ed17a7646792a7167151210451b
MariaPiCruz <cruzmariadelpilar73@gmail.com>
2024-04-05 18:23:08 -0300

    agregado responsive

--- js/script.js
+++ js/script.js
@@ -44,3 +44,22 @@ irTop.addEventListener("click", function () {
     });
 });
 
+/**************************************************/
+/* Evento menu desplegable  */
+/**************************************************/
+let contador = 1;
+function eventoMenu(){
+    const menu = document.querySelector('#menu');
+    const btnMnu = document.querySelector('#btnMnu');
+	if(contador==1){
+		//menu.style='left: 0';
+        menu.style.transform = 'translateX(-0%)';
+		contador=0;
+        btnMnu.innerHTML='x';
+	}else{
+		//menu.style='left:-100%';
+        menu.style.transform = 'translateX(-120%)';
+		contador=1;
+        btnMnu.innerHTML='≡';
+	};
+};
