60e8553fab48609918aede79127d59be603f4851
MariaPiCruz <cruzmariadelpilar73@gmail.com>
2024-04-05 11:07:43 -0300

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+'use strict';
+/*****************************************/
+/* Determina cuando se carga la pagina   */
+/*****************************************/
+window.addEventListener('load', function () {   
+    const loading = document.querySelector('.preloader');
+    loading.classList.add('loaded');
+});
+
+/**********************************************/
+/* Cambia el estilo al menú al hacer scroll   */
+/**********************************************/
+const header=document.querySelector('header');
+const irTop=document.querySelector('#irTop');
+
+// Calcula el punto en el que deseas cambiar el estilo
+const visibleBtnUp =50;
+
+// Agrega un evento de desplazamiento (scroll) al documento
+window.addEventListener('scroll', () => {
+    // Obtiene la posición vertical actual de la página
+    const scrollY = window.scrollY;
+
+    // Comprueba si el usuario ha hecho scroll lo suficiente para cambiar el estilo
+    if (scrollY >= visibleBtnUp) {
+       header.classList.add('header-dark');
+       irTop.classList.remove('active');
+    } else {
+        header.classList.remove('header-dark');
+        irTop.classList.add('active');
+    }
+});
+
+// Evento al hacer click en el boton de ir para arriba
+irTop.addEventListener("click", function () {
+    // Hacemos scroll al tope de la página
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // Esto proporciona un efecto de desplazamiento suave
+    });
+});
+
+
+// Opciones de menú
+function irHome(){
+    
+}
