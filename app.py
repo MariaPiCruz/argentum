7b553f4dbf669229fb9f0266a50f6bd9c38e01bb
milagrosmntrs <milagrosmamonteros@gmail.com>
2024-06-28 02:25:07 -0300

    arreglo de comentarios y agregado de metodos eliminar, modificar y listar un destinatario de agenda

--- app.py
+++ app.py
@@ -65,9 +65,6 @@ class Conexion:
             CONSTRAINT `cliente_tarjeta` FOREIGN KEY (`idTarjeta`) REFERENCES `tarjetas` (`id`)
             ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;''')
         self.conn.commit()
-        # Cerrar el cursor inicial y abrir uno nuevo con el parámetro dictionary=True
-        # self.cursor.close() #no me podes cerrar el cursor aca xq todavia falta ejecutar otra accion
-        # # self.cursor = self.conn.cursor(dictionary=True) #no me podes poner el dictionary en true, porque no estamos manejando los datos como diccionarios.
 
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS cuentas (
             id int NOT NULL AUTO_INCREMENT,
@@ -97,7 +94,6 @@ class Conexion:
         # self.cursor.execute('''INSERT INTO `tarjetas` (nroTarjeta, vencimiento, codigo, tipo, fechaInicio) VALUES ('4000123456789101','2026-08-01','123','VISA Debito','2020-07-01'),('4000987654321011','2026-08-01','987','VISA Credito','2020-07-01'),('377798765444332','2026-08-01','997','AMERICAN EXPRESS','2020-07-01');''')
         # self.conn.commit()
         
-
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS `tipocuentas` (
         `id` int NOT NULL AUTO_INCREMENT,
         `tipoCuenta` varchar(45) NOT NULL,
@@ -109,9 +105,9 @@ class Conexion:
         # self.cursor.execute('''INSERT INTO `tipocuentas` (tipoCuenta, nroCuenta) VALUES ('Caja de Ahorro en Pesos','0650030602000080904070'),('Caja de Ahorro en USD','084-123456 / 3');''')
         # self.conn.commit()
 
-##################################################
-#CREACION TABLA DESTINATARIO
-##################################################
+        # ------------------------------------------------
+        #           CREACION TABLA DESTINATARIO
+        # ------------------------------------------------
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS `destinatarios`(
         `idDestinatario` int NOT NULL AUTO_INCREMENT,
         `destinatario` varchar(45) NOT NULL,
@@ -123,7 +119,7 @@ class Conexion:
         CONSTRAINT `cliente_destinatarios` FOREIGN KEY (`idCliente`) REFERENCES clientes (`id`)
         ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;''')
         self.conn.commit()
-
+        # ------------------------------------------------
 
         self.cursor.close() #el cursor lo cerramos recien aca una vez que se ejecutaron las dos acciones.
         
@@ -132,7 +128,6 @@ class Conexion:
     def agregar_cuenta(self, username, password, idCliente, email):
         sql = "INSERT INTO cuentas (username, password, idCliente, email) VALUES (%s, %s, %s, %s)"
         valores = (username, password, idCliente, email)
-
         self.cursor.execute(sql,valores)
         self.conn.commit()
         nueva_cuenta_id = self.cursor.lastrowid
@@ -140,10 +135,7 @@ class Conexion:
     
     def agregar_cliente(self, nombre, apellido, dni, cuil, cbu, alias, saldo, idTarjeta, idTipoCuenta):
         sql = "INSERT INTO  clientes (nombre, apellido, dni, cuil, cbu, alias, saldo, idTarjeta, idTipoCuenta) values (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-
-
         valores = (nombre, apellido, dni, cuil, cbu, alias, saldo, idTarjeta, idTipoCuenta)
-
         self.cursor.execute(sql,valores)
         self.conn.commit()
         nuevo_cliente_id = self.cursor.lastrowid
@@ -153,7 +145,6 @@ class Conexion:
         self.cursor.execute(f"SELECT * FROM cuentas WHERE username = '{username}' and password = '{password}'")
         return self.cursor.fetchone()
         
-    
     def consultar_cliente_por_id(self, cliente_id):
         sql = "SELECT * FROM clientes WHERE id = %s"
         valores = (cliente_id,)
@@ -164,19 +155,15 @@ class Conexion:
     def modificar_cuenta(self, id, nuevo_username, nuevo_email, nueva_password):
         sql = "UPDATE cuentas SET username = %s, email = %s, password = %s WHERE id = %s"
         valores = (nuevo_username, nuevo_email, nueva_password, id)
-
         self.cursor.execute(sql, valores)
         self.conn.commit()
         return self.cursor.rowcount > 0
         
-
-
     def eliminar_cuenta(self, id):
         self.cursor.execute(f"DELETE FROM cuentas WHERE id = {id}")
         self.conn.commit()
         return self.cursor.rowcount > 0
     
-
     def consultar_datos_cuenta_bancaria(self, idCliente):
         sql = "select tipoCuenta, nroCuenta from clientes, tipoCuentas where tipoCuentas.id = clientes.idTipoCuenta and clientes.id = %s;"
         valores = (idCliente,)
@@ -191,35 +178,56 @@ class Conexion:
         tarjeta = self.cursor.fetchone()
         return tarjeta
     
-##################################################
-#CREACION METODO AGREGAR DESTINATARIO
-##################################################    
+    # ------------------------------------------------
+    #           METODO AGREGAR DESTINATARIO
+    # ------------------------------------------------  
     def agregar_destinatario(self, descripcion,   cbu, alias, idCliente):
         sql = "INSERT INTO destinatarios (descripcion, cbu, alias, idCliente) VALUES (%s, %s, %s, %s)"
         valores = (descripcion, cbu, alias, idCliente)
-
         self.cursor.execute(sql,valores)
         self.conn.commit()
         nuevo_destinatario_id = self.cursor.lastrowid
         return nuevo_destinatario_id
-
-##################################################
-#CREACION METODO MOSTRAR DESTINATARIO
-##################################################
-
+    # ------------------------------------------------   
+    #           METODO MOSTRAR DESTINATARIO
+    # ------------------------------------------------  
     def consultar_destinatarios(self, idCliente):
         sql = "SELECT d.descripcion, d.cbu, d.alias FROM destinatarios d RIGHT JOIN clientes c ON d.idCliente = c.id WHERE idCliente = %s;"
         valores = (idCliente,)
-
         self.cursor.execute(sql, valores)
         destinatarios = self.cursor.fetchall()
         return destinatarios
-    
-##################################################
+    # ------------------------------------------------  
+    #           METODO ELIMINAR DESTINATARIO
+    # ------------------------------------------------ 
+    def eliminar_destinatario(self, idDestinatario):
+        self.cursor.execute(f"DELETE FROM destinatarios WHERE idDestinatario = {idDestinatario}")
+        self.conn.commit()
+        return self.cursor.rowcount > 0
+    # ------------------------------------------------  
+    #           METODO MODIFICAR DESTINATARIO
+    # ------------------------------------------------ 
+    def modificar_destinatario(self, idDestinatario, nueva_descripcion, nuevo_cbu, nuevo_alias):
+        sql = "UPDATE destinatarios SET descripcion = %s, cbu = %s, alias = %s WHERE idDestinatario = %s"
+        valores = (nueva_descripcion, nuevo_cbu, nuevo_alias, idDestinatario)
+        self.cursor.execute(sql, valores)
+        self.conn.commit()
+        return self.cursor.rowcount > 0 
+    # ------------------------------------------------  
+    #           METODO MOSTRAR UN DESTINATARIO
+    # ------------------------------------------------ 
+    def consultar_un_destinatario(self, idDestinatario):
+        sql = "SELECT descripcion, cbu, alias FROM destinatarios WHERE idDestinatario = %s;"
+        valores = (idDestinatario,)
+        self.cursor.execute(sql, valores)
+        destinatarios = self.cursor.fetchall()
+        return destinatarios
 
-# Programa principal
-conexion = Conexion(host='localhost', user='root', password='1234', database='argentum')
 
+# ------------------------------------------------  
+#       PROGRAMA PRINCIPAL
+# ------------------------------------------------ 
+conexion = Conexion(host='localhost', user='root', password='', database='argentum')
 
 @app.route("/cuentas", methods=["POST"])
 def loguin():
@@ -260,15 +268,11 @@ def generar_cbu():
     cbu = f'{base1}{verificador1}{numero_cuenta}{verificador2}'
     return cbu
 
-
-
-
 def generar_alias(longitud_min=6, longitud_max=20):
     caracteres = string.ascii_lowercase + string.digits + '.-'
     longitud = random.randint(longitud_min, longitud_max)
     alias = ''.join(random.choice(caracteres) for _ in range(longitud))
     return alias
-
 
 @app.route("/clientes", methods=["POST"])
 def agregar_cliente():
@@ -282,7 +286,6 @@ def agregar_cliente():
     idTarjeta = 1
     idTipoCuenta = 1
 
-    
     # Agregar el cliente a la base de datos
     nuevo_cliente_id = conexion.agregar_cliente(nombre, apellido, dni, cuil, cbu, alias, saldo, idTarjeta, idTipoCuenta)
 
@@ -311,7 +314,6 @@ def mostrar_datos_cuenta(idCliente):
     else:
         return "Cuenta no encontrada", 404
     
-
 @app.route("/tipoTarjeta/<int:idCliente>", methods=["GET"])
 def mostrar_datos_tarjeta(idCliente):
     tarjeta = conexion.consultar_datos_tarjeta(idCliente)
@@ -319,16 +321,15 @@ def mostrar_datos_tarjeta(idCliente):
         return jsonify(tarjeta)
     else:
         return "Tarjeta no encontrada", 404
-##################################################
-#RUTEO AGREGAR DESTINATARIO
-##################################################
-
+    
+# ------------------------------------------------  
+#           RUTEO AGREGAR DESTINATARIO
+# ------------------------------------------------ 
 @app.route("/destinatarios/<int:idCliente>", methods=["POST"])
 def agregar_destinatario(idCliente):
     descripcion = request.form['descripcion']
     cbu = request.form['cbu']
     alias = request.form['alias']
-    #idCliente = request.args.get('idCliente')
 
     # Agregar el destinatario a la base de datos
     nuevo_destinatario_id = conexion.agregar_destinatario(descripcion,cbu,alias,idCliente)
@@ -339,12 +340,9 @@ def agregar_destinatario(idCliente):
     else:
         return jsonify({"mensaje": "Error al agregar el destinatario."}), 500
 
-
-
-##################################################
-# RUTEO MOSTRAR DESTINATARIO
-##################################################
-
+# ------------------------------------------------  
+#           RUTEO MOSTRAR DESTINATARIO
+# ------------------------------------------------ 
 @app.route("/destinatarios/<int:idCliente>", methods=["GET"])
 def mostrar_agenda(idCliente):
     agenda = conexion.consultar_destinatarios(idCliente)
@@ -353,6 +351,26 @@ def mostrar_agenda(idCliente):
     else:
         return "Agenda vacía", 404
 
+# ------------------------------------------------  
+#           RUTEO ELIMINAR DESTINATARIO
+# ------------------------------------------------
+'''
+@app.route("/destinatarios/<int:idDestinatario>", methods=["DELETE"])
+def eliminar_destinatario(idDestinatario):
+    destinatario = conexion.eliminar_destinatario(idDestinatario)
+    if destinatario:
+        return jsonify({"mensaje": "Contacto eliminado"}), 200
+    else:
+        return jsonify({"mensaje": "Error al eliminar el contacto"}), 500
+'''
+# ------------------------------------------------  
+#           RUTEO MODIFICAR DESTINATARIO
+# ------------------------------------------------ 
+
+
+# ------------------------------------------------  
+#           RUTEO DATOS DE UN DESTINATARIO
+# ------------------------------------------------ 
 
 if __name__ == "__main__":
     app.run(debug=True)
