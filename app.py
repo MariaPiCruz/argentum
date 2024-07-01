28f538e46bb27b467f06486896b822b2ee7a6945
MariaPiCruz <165966135+MariaPiCruz@users.noreply.github.com>
2024-06-30 22:27:03 -0300

    Alias y movimientos

--- app.py
+++ app.py
@@ -102,6 +102,107 @@ class Conexion:
         PRIMARY KEY (`id`)
         ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;''')
         self.conn.commit()
+        
+        # --------------------------------------------------------------
+        # Tabla y datos movimientos
+        # --------------------------------------------------------------
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS `movimientos` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `cliente` varchar(11) COLLATE utf8mb4_general_ci NOT NULL,
+        `nrocuenta` varchar(12) COLLATE utf8mb4_general_ci NOT NULL,
+        `fecha` date NOT NULL,
+        `nrotransaccion` int NOT NULL,
+        `descripcion` varchar(150) COLLATE utf8mb4_general_ci NOT NULL,
+        `importe` decimal(10,2) NOT NULL,
+        `saldo` decimal(10,2) NOT NULL,
+        PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=utf8;''')
+        self.conn.commit()
+        
+        self.cursor.execute('''TRUNCATE TABLE `movimientos`''')
+        self.conn.commit()
+
+        self.cursor.execute('''INSERT INTO `movimientos` (`id`, `cliente`, `nrocuenta`, `fecha`, `nrotransaccion`, `descripcion`, `importe`, `saldo`) VALUES
+        (1, '16', '0650030602000080904070', '2024-05-12', 16733114, 'Compra con tarjeta de debito 	 Farmacia curie - tarj nro. 1669', -9880.00, -1944.17),
+        (2, '16', '0650030602000080904070', '2024-05-13', 42970469, 'Transferencia recibida 	 De severo/carlos          /              - var / 20146301062', 15000.00, 13055.83),
+        (3, '16', '0650030602000080904070', '2024-05-17', 14301551, 'Transferencia recibida 	 De linares  carlos o      /              - fac / 20102285108', 30000.00, 43055.83),
+        (4, '16', '0650030602000080904070', '2024-05-18', 14255271, 'Compra con tarjeta de debito 	 Merpago*beysan - tarj nro. 1669', -11100.00, 25355.83),
+        (5, '16', '0650030602000080904070', '2024-05-18', 12869969, 'Compra con tarjeta de debito 	 Los mussini - tarj nro. 1669', -6600.00, 36455.83),
+        (6, '16', '0650030602000080904070', '2024-05-20', 60647033, 'Pago de servicios 	 C rosales abl: 010000000047393', -5696.01, 8159.82),
+        (7, '16', '0650030602000080904070', '2024-05-20', 10669533, 'Compra con tarjeta de debito 	 Merpago*lanueva - tarj nro. 1669', -6500.00, 13855.83),
+        (8, '16', '0650030602000080904070', '2024-05-20', 31304595, 'Pago de servicios 	 Recarga sube: 6061268259397871', -5000.00, 20355.83),
+        (9, '16', '0650030602000080904070', '2024-05-21', 56497412, 'Compra con tarjeta de debito 	 Cooperativa obrera - tarj nro. 1669', -12750.55, 15409.27),
+        (10, '16', '0650030602000080904070', '2024-05-21', 95832232, 'Transferencia recibida 	 De patricia del pilar mol /              - var / 27208152985', 20000.00, 28159.82),
+        (11, '16', '0650030602000080904070', '2024-05-23', 1104340, 'Seguro de accidentes personales 	 Por deb:23/05/2024 part 000000000001104340', -11849.64, 3559.63),
+        (12, '16', '0650030602000080904070', '2024-05-24', 5511, 'Traspaso de saldo exento de c cte 	 De cuenta corriente a caja de ahorro', 0.00, -2406.57),
+        (13, '16', '0650030602000080904070', '2024-05-24', 5510, 'Traspaso de saldo entre cuentas 	 De cuenta corriente a caja de ahorro', 2406.57, 0.00),
+        (14, '16', '0650030602000080904070', '2024-05-24', 56850750, 'Compra con tarjeta de debito 	 Cooperativa obrera - tarj nro. 1669', -58966.20, -2406.57),
+        (15, '16', '0650030602000080904070', '2024-05-24', 50684054, 'Transferencia recibida 	 De patricia del pilar mol /              - var / 27208152985', 10000.00, 56559.63),
+        (16, '16', '0650030602000080904070', '2024-05-24', 56678281, 'Transferencia recibida 	 De patricia del pilar mol /              - var / 27208152985', 10000.00, 46559.63),
+        (17, '16', '0650030602000080904070', '2024-05-24', 99104910, 'Transferencia inmediata 	 A diego german sanchez    / varios       - var / 20278277489', -7000.00, 36559.63),
+        (18, '16', '0650030602000080904070', '2024-05-24', 98911533, 'Transferencia inmediata 	 A carmen del yessica carr / varios       - var / 27188236540', -10000.00, 43559.63),
+        (19, '16', '0650030602000080904070', '2024-05-24', 95270195, 'Transf recibida cvu mismo titular 	 De nestor david rivarola    / mercado pago   /20183061888', 50000.00, 53559.63),
+        (20, '16', '0650030602000080904070', '2024-05-28', 5514, 'Traspaso de saldo exento de c cte 	 De cuenta corriente a caja de ahorro', 0.00, -9406.57),
+        (21, '16', '0650030602000080904070', '2024-05-28', 5513, 'Traspaso de saldo entre cuentas 	 De cuenta corriente a caja de ahorro', 7000.00, -2406.57),
+        (22, '16', '0650030602000080904070', '2024-05-28', 56034660, 'Transferencia inmediata 	 A diego german sanchez    / varios       - var / 20278277489', -7000.00, -9406.57),
+        (23, '16', '0650030602000080904070', '2024-05-29', 5517, 'Traspaso de saldo entre cuentas 	 De caja de ahorro a cuenta corriente', -9406.57, 70593.43),
+        (24, '16', '0650030602000080904070', '2024-05-29', 5516, 'Traspaso de saldo exento a c cte 	 De caja de ahorro a cuenta corriente', 0.00, 80000.00),
+        (25, '16', '0650030602000080904070', '2024-05-29', 60105924, 'Transferencia recibida 	 De rivarola/nestor dav    /              - var / 20183061888', 80000.00, 70593.43),
+        (26, '16', '0650030602000080904070', '2024-05-30', 5522, 'Traspaso de saldo exento de c cte 	 De cuenta corriente a caja de ahorro', 0.00, -85244.07),
+        (27, '16', '0650030602000080904070', '2024-05-30', 5521, 'Traspaso de saldo entre cuentas 	 De cuenta corriente a caja de ahorro', 85244.07, 0.00),
+        (28, '16', '0650030602000080904070', '2024-05-30', 56790838, 'Compra con tarjeta de debito 	 Cooperativa obrera - tarj nro. 1669', -126803.50, -85244.07),
+        (29, '16', '0650030602000080904070', '2024-05-30', 11107692, 'Compra con tarjeta de debito 	 Panaderia lujan - tarj nro. 1669', -20034.00, 41559.43),
+        (30, '16', '0650030602000080904070', '2024-05-30', 8555147, 'Pago de servicios 	 Recarga sube: 6061268259397871', -9000.00, 61593.43),
+        (31, '16', '0650030602000080904070', '2024-05-31', 5531, 'Traspaso de saldo entre cuentas 	 De caja de ahorro a cuenta corriente', -85244.07, 22287.43),
+        (32, '16', '0650030602000080904070', '2024-05-31', 5530, 'Traspaso de saldo exento a c cte 	 De caja de ahorro a cuenta corriente', 0.00, 107531.50),
+        (33, '16', '0650030602000080904070', '2024-05-31', 1346030, 'Compra con tarjeta de debito 	 Bar central - tarj nro. 1669', -33800.00, 22287.43),
+        (34, '16', '0650030602000080904070', '2024-05-31', 4454, 'Retiro de efectivo en otros bancos 	 Tarj nro. 1669', -5000.00, 56087.43),
+        (35, '16', '0650030602000080904070', '2024-05-31', 16105372, 'Compra con tarjeta de debito 	 Vantage fcia del puert - tarj nro. 1669', -18218.50, 61087.43),
+        (36, '16', '0650030602000080904070', '2024-05-31', 296946, 'Transferencia recibida 	 De sociedad militar segur / 5033425      - hab / 30527516737', 100000.00, 79305.93),
+        (37, '16', '0650030602000080904070', '2024-05-31', 1982143, 'Compra con tarjeta de debito 	 Forraje don sebastian - tarj nro. 1669', -115450.00, -20694.07),
+        (38, '16', '0650030602000080904070', '2024-05-31', 27253, 'Transferencia recibida 	 De sociedad militar segur / 5031023      - hab / 30527516737', 100000.00, 94755.93),
+        (39, '16', '0650030602000080904070', '2024-05-31', 52330438, 'Transferencia recibida 	 De rivarola/nestor dav    /              - var / 20183061888', 80000.00, -5244.07),
+        (40, '16', '0650030602000080904070', '2024-06-02', 6480700, 'Transferencia recibida 	 De rivarola/nestor dav    /              - var / 20183061888', 150000.00, 172287.43),
+        (41, '16', '0650030602000080904070', '2024-06-03', 1117296, 'Pago a proveedores recibido 	 Jaque jonathan                20393542080 03 1117296', 100000.00, 177497.44),
+        (42, '16', '0650030602000080904070', '2024-06-03', 18880824, 'Compra con tarjeta de debito 	 Chop chop - tarj nro. 1669', -12000.00, 77497.44),
+        (43, '16', '0650030602000080904070', '2024-06-03', 15425725, 'Compra con tarjeta de debito 	 Level games - tarj nro. 1669', -6000.00, 89497.44),
+        (44, '16', '0650030602000080904070', '2024-06-03', 84153, 'Debito automatico 	 Personal flow da-1001931994010002', -76789.99, 95497.44),
+        (45, '16', '0650030602000080904070', '2024-06-04', 16140270, 'Compra con tarjeta de debito 	 Merpago*wirooscloudho - tarj nro. 1669', -16222.80, 118922.24),
+        (46, '16', '0650030602000080904070', '2024-06-04', 67025767, 'Transferencia realizada 	 A asoc coop esc tecnica n / varios       - var / 33688086049', -15000.00, 135145.04),
+        (47, '16', '0650030602000080904070', '2024-06-04', 56628017, 'Compra con tarjeta de debito 	 Cooperativa obrera - tarj nro. 1669', -16817.50, 150145.04),
+        (48, '16', '0650030602000080904070', '2024-06-04', 56627490, 'Compra con tarjeta de debito 	 Cooperativa obrera - tarj nro. 1669', -83534.90, 166962.54),
+        (49, '16', '0650030602000080904070', '2024-06-04', 60748865, 'Transferencia inmediata 	 A diego german sanchez    / varios       - var / 20278277489', -7000.00, 250497.44),
+        (50, '16', '0650030602000080904070', '2024-06-04', 8257937, 'Transferencia recibida 	 De rivarola/nestor dav    /              - var / 20183061888', 80000.00, 257497.44),
+        (51, '16', '0650030602000080904070', '2024-06-05', 9304, 'Pago de haberes 	 Transferencia s.n.p.', 691476.00, 796398.24),
+        (52, '16', '0650030602000080904070', '2024-06-05', 21338803, 'Compra con tarjeta de debito 	 Matias jr - tarj nro. 1669', -23700.00, 104922.24),
+        (53, '16', '0650030602000080904070', '2024-06-05', 807662, 'Transferencia recibida 	 De c a s tecnologia y seg /              - fac / 30709810770', 65000.00, 128622.24),
+        (54, '16', '0650030602000080904070', '2024-06-05', 61423713, 'Transferencia inmediata 	 A patricia del pilar moli / varios       - var / 27208152985', -20000.00, 63622.24),
+        (55, '16', '0650030602000080904070', '2024-06-05', 34289072, 'Transferencia realizada 	 A patricia del pilar moli / varios       - var / 27208152985', -30000.00, 83622.24),
+        (56, '16', '0650030602000080904070', '2024-06-05', 10747264, 'Compra con tarjeta de debito 	 Panaderia lujan - tarj nro. 1669', -5300.00, 113622.24),
+        (57, '16', '0650030602000080904070', '2024-06-06', 15108532, 'Compra con tarjeta de debito 	 Pibro sud srl - tarj nro. 1669', -48627.00, 469563.01),
+        (58, '16', '0650030602000080904070', '2024-06-06', 2783900, 'Transferencia realizada 	 A rivarola emiliano javie / varios       - var / 20410728940', -30000.00, 518190.01),
+        (59, '16', '0650030602000080904070', '2024-06-06', 9658135, 'Pago de servicios 	 Gas pampeana: 81090220100613841', -8760.69, 548190.01),
+        (60, '16', '0650030602000080904070', '2024-06-06', 2074856, 'Compra con tarjeta de debito 	 Www.camuzzi gas pampeana - tarj nro. 1669', -8760.69, 556950.70),
+        (61, '16', '0650030602000080904070', '2024-06-06', 87943446, 'Pago de servicios 	 Absa: 0001607487', -858.89, 565711.39),
+        (62, '16', '0650030602000080904070', '2024-06-06', 39237113, 'Pago de servicios 	 Absa: 0001607487', -858.89, 566570.28),
+        (63, '16', '0650030602000080904070', '2024-06-06', 76766748, 'Pago de servicios 	 Tarj naranja: 5895629993490781', -228969.07, 567429.17),
+        (64, '16', '0650030602000080904070', '2024-06-07', 5566, 'Impuesto ley 25.413 debito 0,6% 	 ', 0.00, 477595.41),
+        (65, '16', '0650030602000080904070', '2024-06-07', 5565, 'Traspaso de saldo entre cuentas 	 De caja de ahorro a cuenta corriente', -368.59, 477597.61),
+        (66, '16', '0650030602000080904070', '2024-06-07', 5564, 'Traspaso de saldo exento a c cte 	 De caja de ahorro a cuenta corriente', 0.00, 477966.20),
+        (67, '16', '0650030602000080904070', '2024-06-07', 62214534, 'Transferencia realizada 	 A lamique sebastian mario / varios       - var / 20402247615', -3600.00, 477597.61),
+        (68, '16', '0650030602000080904070', '2024-06-07', 17307896, 'Compra con tarjeta de debito 	 Merpago*micolmartin - tarj nro. 1669', -18000.00, 481197.61),
+        (69, '16', '0650030602000080904070', '2024-06-07', 52772327, 'Transferencia recibida 	 De rivarola/nestor dav    / varios       - var / 20183061888', 80000.00, 499197.61),
+        (70, '16', '0650030602000080904070', '2024-06-07', 63564656, 'Transferencia inmediata 	 A patricia del pilar moli / varios       - var / 27208152985', -50000.00, 419197.61),
+        (71, '16', '0650030602000080904070', '2024-06-07', 5559, 'Impuesto de sellos 	 ', 0.00, 469197.61),
+        (72, '16', '0650030602000080904070', '2024-06-07', 5558, 'Iva 21% 	 ', 0.00, 469201.04),
+        (73, '16', '0650030602000080904070', '2024-06-07', 5557, 'Cobro de interes por descubierto 	 Del 01/05/24 al 31/05/24', 0.00, 469264.03),
+        (74, '16', '0650030602000080904070', '2024-06-07', 5556, 'Pago interes por saldo en cuenta 	 Del 01/05/24 al 31/05/24', 0.99, 469564.00),
+        (75, '16', '0650030602000080904070', '2024-06-08', 17351719, 'Compra con tarjeta de debito 	 Cocina casera - tarj nro. 1669', -18600.00, 458995.41),
+        (76, '16', '0650030602000080904070', '2024-06-09', 89945065, 'Compra con tarjeta de debito 	 Personal flow - tarj nro. 1669', -26570.04, 411729.36),
+        (77, '16', '0650030602000080904070', '2024-06-09', 65336543, 'Transferencia inmediata 	 A leonardo alberto quirog / varios       - var / 20378963649', -15000.00, 438299.40),
+        (78, '16', '0650030602000080904070', '2024-06-09', 7941407, 'Pago de servicios 	 C rosales abl: 010000000047393', -5696.01, 453299.40),
+        (79, '16', '0650030602000080904070', '2024-06-10', 11729499, 'Compra con tarjeta de debito 	 Merpago*mercadopagopag - tarj nro. 1669', -19597.21, 392132.15);''')
+        self.conn.commit()
 
         # self.cursor.execute('''INSERT INTO `tipocuentas` (tipoCuenta, nroCuenta) VALUES ('Caja de Ahorro en Pesos','0650030602000080904070'),('Caja de Ahorro en USD','084-123456 / 3');''')
         # self.conn.commit()
@@ -171,6 +272,27 @@ class Conexion:
         self.cursor.execute(sql, valores)
         tarjeta = self.cursor.fetchone()
         return tarjeta
+
+    # -------------------------------------------------
+    # Alias y movimientos
+    # -------------------------------------------------
+    def actualiza_alias(self, idCliente, alias):
+        sql = "update clientes set alias=%s where id=%s;"
+        valores = (alias, idCliente,)
+        self.cursor.execute(sql, valores)
+        self.conn.commit()    
+        return self.cursor.rowcount > 0
+    
+    def getMovimientos(self, client, cuenta):
+        sql = "SELECT id, cliente, nrocuenta, fecha, nrotransaccion, descripcion, importe, saldo FROM movimientos WHERE cliente=%s AND nrocuenta=%s ORDER BY fecha DESC;"
+        self.cursor.execute(sql, (client,cuenta,))
+
+        movimientos = self.cursor.fetchall()
+
+        if movimientos:
+            return movimientos
+        else:
+            return ("No se encontraron movimientos en esta cuenta")
 
 # Programa principal
 conexion = Conexion(host='localhost', user='root', password='1234', database='argentum')
@@ -278,6 +400,20 @@ def mostrar_datos_tarjeta(idCliente):
     else:
         return "Tarjeta no encontrada", 404
 
+@app.route("/alias", methods=["PUT"])
+def cambiar_alias():
+    cliente = request.form['id']
+    alias = request.form['alias']
+    alias = conexion.actualiza_alias(cliente, alias)
+    if alias:
+        return jsonify(alias)
+    else:
+        return "Se produjo un error al actualizar el alias", 500
+    
+@app.route('/movimientos/<string:client>/<string:cuenta>', methods=['GET'])
+def obtener_movimientos(client,cuenta):
+    movimientos = conexion.getMovimientos(client,cuenta)
+    return jsonify(movimientos)
 
 
 if __name__ == "__main__":
