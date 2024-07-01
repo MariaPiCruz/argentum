2a742838eaf38ded589cd306a4a521bd4ea081c9
MariaPiCruz <165966135+MariaPiCruz@users.noreply.github.com>
2024-07-01 18:09:01 -0300

    Update inicio.js

--- static/js/inicio.js
+++ static/js/inicio.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const url = "https://maripilicruz.pythonanywhere.com/";
+const URL = "https://maripilicruz.pythonanywhere.com/";
 //const URL = "http://127.0.0.1:5000/";
 // const URL = "JensonMedina.pythonanywhere.com/";
 
